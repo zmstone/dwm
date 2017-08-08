40dfe4aa021a397bce3e55f9a25b3f3ff0e5a631
William Yang <mscame@gmail.com>
2017-08-08 23:34:30 +0200

    copyq menu

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
-static const char *clipcmd[]  = { "copyq", "toggle", NULL };
+static const char *clipcmd[]  = { "copyq", "menu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
