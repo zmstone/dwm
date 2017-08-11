ba2b0fc55eab7e17091f138c5b0d13956b3cab3c
William Yang <mscame@gmail.com>
2017-08-11 17:33:57 +0200

    fix emacs tagging

--- config.h
+++ config.h
@@ -34,7 +34,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 3,       0,           -1 },
   { "copyq",    NULL,       NULL,       0,            1,           -1 },
-  { "emacs",    NULL,       NULL,       1 << 1,       0,           -1 },
+  { "Emacs",    NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
