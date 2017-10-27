068fdbce127fb1f8b0178cf4e518b03de8efe1f3
Zaiming Shi <zmstone@gmail.com>
2017-10-27 21:16:47 +0200

    Use win key as mod key

--- config.def.h
+++ config.def.h
@@ -42,7 +42,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask /* Mod1Mask is alt Mod4Mask is win key */
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
